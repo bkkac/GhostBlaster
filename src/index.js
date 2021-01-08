9fc9f2f5f90ea83dc7c2c28b8a6416102ff0a3f6
Jae Doherty <jaeld97@gmail.com>
2021-01-08 14:41:32 -0800

    added music

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import GhostBlasters from './game';
 // import '../stylesheets/application.css';
 
-const canvas = document.getElementById('game-canvas');
-new GhostBlasters(canvas);
 // console.log("webpack is working")
+
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('game-canvas');
+    new GhostBlasters(canvas);
+ 
+
+
+    const sound = document.getElementById('sound');
+    const soundOn = document.getElementById('sound-on')
+    const soundOff = document.getElementById('sound-off')
+
+
+    soundOn.addEventListener("click", () => {
+        sound.play();
+    })
+
+    soundOff.addEventListener('click', () => {
+        sound.pause();
+    })
+
+
+
+})
