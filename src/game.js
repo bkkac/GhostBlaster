d5dbcb9a0669a7ca247a51693fd9dc37423f7147
Jae Doherty <jaeld97@gmail.com>
2021-01-08 10:55:10 -0800

    fixed continual addition of ghosts

--- src/game.js
+++ src/game.js
@@ -1,13 +1,14 @@
 import Level from "./level";
 import Shooter from "./shooter";
 import Bullet from "./bullet";
+import Ghost from "./ghost";
 
 export default class GhostBlasters {
     constructor(canvas) {
         this.ctx = canvas.getContext("2d");
         this.canvas = canvas;
         this.dimensions = { width: canvas.width, height: canvas.height };
-        this.ghost_gen = 1;
+        this.interval = 5000;
         this.bullets = [];
         this.ghosts = [];
         this.creepster = new FontFace(
@@ -30,7 +31,9 @@ export default class GhostBlasters {
         this.level = new Level(this.dimensions);
         this.shooter = new Shooter (this.dimensions);
         this.bullet = new Bullet(this.dimensions);
-
+        this.ghost = new Ghost(this.dimensions);
+        this.ghosts.push(this.ghost);
+ 
         this.animate(); 
     }
 
@@ -45,8 +48,11 @@ export default class GhostBlasters {
         }
 
         this.level.moveHouse();
-        this.level.ghosts.push(this.level.randomGhost())
-        this.level.moveGhosts();
+        this.ghostInterval();
+        // this.ghosts.forEach(ghost => ghost.animate(this.ctx))
+        
+        // this.level.ghostInterval();
+        // this.ghost.moveGhost();
         
         
     }
@@ -60,20 +66,40 @@ export default class GhostBlasters {
             this.bullets.shift();
         }
         
-        // console.log("game", this.bullet);
-        // const bullet = this.bullet.newBullet;
-        // console.log("before", this.bullet.bullets) 
         const bullet = new Bullet();
         this.bullets.push(bullet);
-        // console.log("after", this.bullet.bullets)
 
-        // this.bullet.animate(this.ctx);
  
         const x = e.clientX;
         const y = e.clientY;
-        // console.log(x, y)
        
         bullet.moveBullet(x, y); 
+    }
+
+    ghostInterval() {
+        if (this.ghosts.length % 7 === 0) {
+            this.ghosts.shift();
+            this.interval -= 500;
+        }
+
+        setInterval(this.addGhost.bind(this), this.interval)
+        
+        // console.log(this.ghos ts);
+
+
+    }
+
+    addGhost(count) {
+        for (let ghost = 0; ghost < count; ghost++) {
+            const ghost = new Ghost(this.dimensions)
+            this.ghosts.push(ghost)
+            
+        }
+        const ghost = new Ghost(this.dimensions) 
+        this.ghosts.push(ghost);
+        ghost.animate(this.ctx);
+        console.log(this.ghosts);
+        
     }
 
 
@@ -83,7 +109,7 @@ export default class GhostBlasters {
         ctx.drawImage(sparkles, 180, 725  , 100, 100)
         sparkles.onload = function () {
 
-            ctx.drawImage(sparkles, 180, 725  , 100, 100)
+            ctx.drawImage(sparkles, 180, 725, 100, 100)
         }
     }
  
@@ -91,6 +117,7 @@ export default class GhostBlasters {
         this.ctx.clearRect(0, 0, this.dimensions.width, this.dimensions.height);
         this.level.animate(this.ctx);
         this.shooter.animate(this.ctx);
+        this.ghost.animate(this.ctx);
         
         
         if (!this.running) {
@@ -100,7 +127,9 @@ export default class GhostBlasters {
             this.startGame();
         }
         if (this.running) {
+            // this.ghostInterval();
             this.bullets.forEach(bullet => bullet.animate(this.ctx));
+            this.ghosts.forEach(ghost => ghost.animate(this.ctx));
             requestAnimationFrame(this.animate.bind(this));
             this.drawSparkles(this.ctx);
             
--- src/ghost.js
+++ src/ghost.js
@@ -7,63 +7,60 @@ const CONSTANTS = {
     WARM_UP_SECONDS: 1
 };
 
-
-const CONSTANTS = {
-    GHOST_WIDTH: 63,
-    GHOST_HEIGHT: 70,
-    EDGE_BUFFER: 50,
-    WARM_UP_SECONDS: 1
-};
 export default class Ghost {
 
     constructor(dimensions) {
-        this.velocity = 5;
+        this.velocity = 2;
         this.dimensions = dimensions;
-        this.x = dimensions.width / 3;
-        this.y = this.dimensions.height / 2;
-        this.ghost_x = 635;
+        this.y = Math.floor(Math.random() * 551) + 10;
+        this.x = Math.floor(Math.random() * 1601) + 1200;
     }
+
 
     newGhost(ctx) {
 
         var unshotGhost = new Image();
         unshotGhost.src = '../images/unshot_ghost.png';
+        ctx.drawImage(unshotGhost, this.x, this.y, 63, 70)
 
+        let that = this;
         unshotGhost.onload = function() {
 
-            ctx.drawImage(unshotGhost, 50, this.ghost_x, 63, 70)
+            ctx.drawImage(unshotGhost, that.x, that.y, 63, 70)
         }
     }
 
-    shotGhost(ctx) {
+    shotGhost(ctx, pos) {
 
         var shotGhost = new Image();
         shotGhost.src = '../images/shot_ghost.png';
-
+        ctx.drawImage(shotGhost, this.x, this.y, 63, 70)
+        let that = this;
         shotGhost.onload = function () {
 
-            ctx.drawImage(shotGhost, 50, 35, this.ghost_x, 70)
+            ctx.drawImage(shotGhost, that.x, that.y, 63, 70)
         }
     }
 
-    drawGhost() {
-        let unshotGhost = new Image();
-        unshotGhost.src = './images/unshot_ghost.png';
-        ctx.drawImage(unshotGhost, ghost.left, ghost.top, CONSTANTS.GHOST_WIDTH, CONSTANTS.GHOST_HEIGHT)
+    deadGhost(ctx, pos) {
+        var deadGhost = new Image();
+        deadGhost.src = '../images/deadghost.png';
+        ctx.drawImage(deadGhost, this.x, this.y, 63, 70)
+        let that = this;
+        deadGhost.onload = function () {
 
-        unshotGhost.onload = function () {
+            ctx.drawImage(deadGhost, that.x, that.y, 63, 70)
+        } 
+     }
 
-            ctx.drawImage(unshotGhost, ghost.left, ghost.top, CONSTANTS.GHOST_WIDTH, CONSTANTS.GHOST_HEIGHT)
-    }
 
     moveGhost(ctx) {
-        this.ghost_x -= this.velocity;
+        this.x -= this.velocity;
     }
 
     animate(ctx) {
         this.newGhost(ctx);
         this.moveGhost(ctx);
-        // this.shotGhost(ctx);
     }
 
 
